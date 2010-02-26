4a42c7b04e1690988170e586e050154da1778df4
angelabohn (Angela Bohn) <angela.bohn@gmail.com>
2010-02-26 07:17:38 +0000

    Removed file/folder
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/snadata@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/data/taskviews.r
+++
@@ -1,8 +0,0 @@
-taskviews <- function(){
-    views <- .readRDS(url("http://CRAN.R-project.org/src/contrib/Views.rds"))
-    do.call("rbind",
-            mapply(cbind,
-                   Package =
-                   lapply(views, function(v) v$packagelist$name),
-                   View = sapply(views, "[[", "name")))
-}
